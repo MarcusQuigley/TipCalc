0e067368836c67549af096049a8bccb00d922322
MarcusQuigley <marcusquigley@gmail.com>
2018-06-30 17:20:01 -0400

    Wpf branch (#1)
    * Inital project creation; Creation of startup func
    
    * Adding view and creating setup logic

---
+++ TipCalc.UI.Wpf/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using MvvmCross.Wpf.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TipCalc.UI.Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        bool _setupComplete;
+
+        void DoSetup()
+        {
+            var presenter = new MvxSimpleWpfViewPresenter(MainWindow);
+
+            var setup = new Setup(this.Dispatcher, presenter);
+            setup.Initialize();
+
+            var start = Mvx.Resolve<IMvxAppStart>();
+            start.Start();
+
+            _setupComplete = true;
+        }
+
+        protected override void OnActivated(EventArgs e)
+        {
+            if (!_setupComplete)
+            {
+                DoSetup();
+            }
+            base.OnActivated(e);
+        }
+
+    }
+
+}
---
+++ TipCalc.UI.Wpf/Setup.cs
@@ -0,0 +1,26 @@
+﻿using MvvmCross.Wpf.Platform;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Core.ViewModels;
+using System.Windows.Threading;
+using MvvmCross.Wpf.Views;
+ 
+namespace TipCalc.UI.Wpf
+{
+   public class Setup : MvxWpfSetup
+    {
+        public Setup(Dispatcher uiThreadDispatcher, IMvxWpfViewPresenter presenter)
+            :base(uiThreadDispatcher, presenter)
+        {
+            
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new Core.App();
+        }
+    }
+}
