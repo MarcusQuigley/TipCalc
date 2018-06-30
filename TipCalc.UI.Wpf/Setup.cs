053da203013180f85567e6516352bbbc4b1595ec
[MarcusQ] <marcusquigley@gmail.com>
2018-06-30 16:42:53 -0400

    Inital project creation; Creation of startup func

---
+++ TipCalc.UI.Wpf/Setup.cs
@@ -0,0 +1,26 @@
+﻿using MvvmCross.Wpf.Platform;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Core.ViewModels;
+using System.Windows.Threading;
+using MvvmCross.Wpf.Views;
+using MvvmCross.Core.ViewModels;
+namespace TipCalc.UI.Wpf
+{
+   public class Setup : MvxWpfSetup
+    {
+        public Setup(Dispatcher uiThreadDispatcher, IMvxViewPresenter presenter)
+            :base(uiThreadDispatcher, presenter)
+        {
+            
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
