c84891ae34a732f93d767d66161f7c3180775cc2
[MarcusQ] <marcusquigley@gmail.com>
2018-07-01 13:50:00 -0400

    basic navigation with new views

--- TipCalc.Core/ViewModels/LoginViewModel.cs
+++ TipCalc.Core/ViewModels/LoginViewModel.cs
@@ -2,10 +2,31 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace TipCalc.Core.ViewModels
 {
-  public class LoginViewModel : MvxViewModel
+    public class LoginViewModel : MvxViewModel
     {
+        private string _id;
+
+        public ICommand LoginCommand
+        {
+            get => new MvxCommand(() => base.ShowViewModel<TipViewModel>());
+        }
+
+        public string Id
+        {
+            get => _id;
+            set
+            {
+                if (_id != value)
+                {
+                    _id = value;
+                    base.RaisePropertyChanged(() => Id);
+                 
+                }
+            }
+        }
     }
 }
